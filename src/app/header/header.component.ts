4fb242546a796868816741318fbd65ecbaa13bf0
Nellia Salimova <salimova.nellia@gmail.com>
2020-04-27 22:50:36 +0300

    Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  burgerReleased: boolean = false;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
 
+  toggleMenu() {
+    this.burgerReleased = !this.burgerReleased;
+  }
 }
